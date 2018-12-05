97f105cc1855d6bbb55ce75ee93803ba874928fe
Cooper Bell <john.cooper.bell@gmail.com>
2018-12-05 08:45:36 -0600

    Add day 5 part 1

---
+++ day5/part1.lisp
@@ -0,0 +1,27 @@
+(defun read-polymer (path)
+  (with-open-file (in path)
+    (read-line in nil nil)))
+
+(defvar *polymer* (read-polymer "input"))
+
+(defun oppositePolarityp (a b)
+  (let ((inta (char-int a))
+        (intb (char-int b)))
+    (= (+ (min inta intb) 32) (max inta intb))))
+
+(defun simplify-polymer-helper (polymer processed)
+  (if polymer
+      (cond ((and (consp processed)
+                  (oppositepolarityp (first polymer) (first processed)))
+             (simplify-polymer-helper (cdr polymer) (cdr processed)))
+            ((and (consp polymer) (consp (cdr polymer))
+                  (oppositepolarityp (first polymer) (second polymer)))
+             (simplify-polymer-helper (cddr polymer) processed))
+            (t (destructuring-bind (head . rest) polymer
+                 (simplify-polymer-helper rest (cons head processed)))))
+      processed))
+
+(defun simplify-polymer (&optional (polymer (coerce *polymer* 'list)))
+  (coerce (reverse (simplify-polymer-helper polymer nil)) 'string))
+
+(format t "Remaining units: ~a~%" (length (simplify-polymer)))
